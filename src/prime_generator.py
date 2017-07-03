f80ceeb6e76c752d2f54b6ffe079ab73194db310
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-03 19:16:31 +0300

    added test for correct output

--- src/prime_generator.py
+++ src/prime_generator.py
@@ -3,3 +3,22 @@ def prime_numbers(n):
     if n < 0:
         return None
 
+    primes = []
+
+    for i in range(2, n):
+
+        # assume number to be prime
+        isPrime = True
+
+        # all numbers from 2 to checked
+        for j in range(2, i):
+
+            # if we find number to be divisible, not prime
+            if i % j == 0:
+                isPrime = False
+                break
+
+        if isPrime:
+            primes.append(i)
+
+    return primes
--- tests/test_prime_generator.py
+++ tests/test_prime_generator.py
@@ -6,3 +6,7 @@ from src.prime_generator import prime_numbers
 def test__prime_numbers_returns_none_for_negative_input__succeeds():
     primes = prime_numbers(-3)
     assert primes is None
+
+def test__prime_numbers_returns_correct_output__succeeds():
+    primes = prime_numbers(6)
+    assert primes == [2, 3, 5]
