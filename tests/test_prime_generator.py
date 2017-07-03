bba9fb154c1577f45ed1c5ca8c5327d4c81a7004
Arnold Taremwa <taremwaarnold@gmail.com>
2017-07-03 19:26:30 +0300

    added test for input zero

--- tests/test_prime_generator.py
+++ tests/test_prime_generator.py
@@ -7,13 +7,21 @@ def test__prime_numbers_returns_none_for_negative_input__succeeds():
     primes = prime_numbers(-3)
     assert primes is None
 
+
+def test__prime_numbers_returns_empty_list_for_0_input():
+    primes = prime_numbers(0)
+    assert primes == []
+
+
 def test__prime_numbers_returns_correct_output__succeeds():
     primes = prime_numbers(6)
     assert primes == [2, 3, 5]
 
+
 def test__prime_numbers_returns_n_inclusive_if_prime__succeeds():
     primes = prime_numbers(7)
     assert primes == [2, 3, 5, 7]
+
 
 def test__prime_numbers_str_input__raises():
     with pytest.raises(TypeError):
